a1bb6c973244635ac55f494c6c5cf9b40af967fc
Yonathan Wolowelsky <grmmph@gmail.com>
2020-10-05 21:23:18 +0300

    init

--- components/alert.js
+++ components/alert.js
@@ -25,7 +25,7 @@ export default function Alert({ preview }) {
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
+
               <a
                 href={`https://github.com/zeit/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
                 className="underline hover:text-success duration-200 transition-colors"

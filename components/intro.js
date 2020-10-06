d7c7609dfada757a63fb4fd3f95edac8650e86d9
Yonathan Wolowelsky <grmmph@gmail.com>
2020-10-06 19:06:16 +0300

    add analyticsg

--- components/intro.js
+++ components/intro.js
@@ -2,12 +2,15 @@ import { CMS_NAME, CMS_URL } from '@/lib/constants'
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
-        Trends Everywhere Blog
+    <section className="items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-7xl text-accent-4 md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
+      <img style={{width: 70,
+            display: 'inline-block',
+            marginRight: 20,
+            }} src="/blog/trends-everywhere-icon.png"></img> Trends Everywhere Blog
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        How to hunt and analyse internet trends before the explode
+      <h4 className="text-accent-3 text-center md:text-left text-2xl mt-5">
+        How to hunt and analyse Internet trends before the explode
       </h4>
     </section>
   )
